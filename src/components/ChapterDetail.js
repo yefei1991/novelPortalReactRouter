1821d86046f9b0dacf77a16f56d7a373e39ae877
yefei <2360274407@qq.com>
2019-02-14 13:43:51 +0800

    添加上一页下一页功能

--- src/components/ChapterDetail.js
+++ src/components/ChapterDetail.js
@@ -1,26 +1,52 @@
-import React from "react"
+import React from 'react';
 import axios from 'axios';
+import {Link} from 'react-router-dom';
 
-class ChapterDetail extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            chapter: '',
-        };
+class ChapterDetail extends React.Component {
+  constructor (props) {
+    super (props);
+    this.state = {
+      chapter: {},
+    };
+  }
+  componentDidMount () {
+    axios
+      .get (`/api/chapterDetail?chapterId=${this.props.match.params.chapterId}`)
+      .then (res => {
+        console.info (res);
+        this.setState ({chapter: res.data.data});
+      });
+  }
+  subUrl(str){
+    var index = str .lastIndexOf("\/")
+    str  = str .substring(0, index)
+    return str
+  }
+  render () {
+    let prevLink=null
+    let nextLink=null
+    var oriUrl=this.subUrl(this.props.match.url);
+    if(this.state.chapter.prev){
+        prevLink=<Link target="_href" to={`${oriUrl}/${this.state.chapter.prev}`}>上一页</Link>
     }
-    componentDidMount() {
-        axios.get(`/api/chapterDetail?chapterId=${this.props.match.params.chapterId}`).then(res => {
-            console.info(res)
-            this.setState({ chapter: res.data.data.content });
-        });
+    if(this.state.chapter.next){
+        nextLink=<Link target="_href" to={`${oriUrl}/${this.state.chapter.next}`}>下一页</Link>
     }
-    render() {
-        return (
-            <div>
-                {this.state.chapter}
-            </div>
-        );
-    }
+    return (
+      <div style={{width: '800px', margin: '0 auto', padding: '50px 0 50px 0'}}>
+        {this.state.chapter.content}
+        <div
+          style={{
+            width: '200px',
+            margin: '20px auto 0 auto',
+            textAlign: 'center',
+          }}
+        >
+        {prevLink}{" "}{nextLink}
+        </div>
+      </div>
+    );
+  }
 }
 
-export default ChapterDetail
+export default ChapterDetail;
